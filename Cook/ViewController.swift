1ca0c5749f2ff6e4b3be5d1c529a429e7b944ff1
bigboateng2011@gmail.com <bigboateng2011@gmail.com>
2017-11-13 04:51:17 +0000

    Starting working on iOS swift tutorial.

--- Cook/ViewController.swift
+++ Cook/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
 
     override func viewDidLoad() {
+
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func setDefaultLabelText(_ sender: Any) {
+        titleLabel.text = "Hello world"
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        titleLabel.text = textField.text
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard, text field should resign its first-responder status when the user taps a button to end editing in the text field.
+        textField.resignFirstResponder()
+        return true
+    }
 
 
 }
-
+ 
